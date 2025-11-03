55080825b300ee76e0f0786336dcbcd81653e864
pphuongnguyenDevweb <pphuongnguyen134@gmail.com>
2025-11-03 18:04:00 +0700

    Create CashTransaction.js

---
+++ models/CashTransaction.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const cashTransactionSchema = new mongoose.Schema({
+  transactionId: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  amount: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('CashTransaction', cashTransactionSchema);
